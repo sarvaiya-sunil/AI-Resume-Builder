d28198510c350c4856d84185dc67ba74141dc74e
sarvaiya-sunil <sarvaiyasunil25@gmail.com>
2026-07-03 17:44:50 +0530

    created personal information form & template/color selecter

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -4,6 +4,7 @@ import { dummyResumeData } from "../assets/assets";
 import {
   ArrowLeftIcon,
   Briefcase,
+  ChevronLeft,
   ChevronRight,
   FileText,
   FolderIcon,
@@ -11,6 +12,10 @@ import {
   Sparkles,
   User,
 } from "lucide-react";
+import PersonalInfoForm from "../components/PersonalInfoForm";
+import ResumePreview from "../components/ResumePreview";
+import TemplateSelector from "../components/TemplateSelector";
+import ColorPicker from "../components/ColorPicker";
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams();
@@ -70,20 +75,36 @@ const ResumeBuilder = () => {
       <div className="max-w-7xl mx-auto px-4 pb-8">
         <div className="grid lg:grid-cols-12 gap-8">
           {/* Left panel - form */}
-          <div className="relative lg:col-span-5 rounded-lg overflow-hidden">
-            <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1">
+          <div className="lg:col-span-5 rounded-lg overflow-hidden">
+            <div className="relative bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1">
               {/* Progress bar using activeSectionIndex */}
               <hr className="absolute top-0 left-0 right-0 border-2 border-gray-200" />
               <hr
                 className="absolute top-0 left-0 h-1 bg-gradient-to-r from-green-500 to-green-600 border-none transition-all duration-2000"
                 style={{
-                  width: `${(activeSectionIndex * 100) / (sections.length - 1)}`,
+                  width: `${(activeSectionIndex * 100) / (sections.length - 1)}%`,
                 }}
               />
 
               {/* Section Navigation */}
               <div className="flex items-center justify-between mb-6 border-b border-gray-300 py-1">
-                <div className=""></div>
+                <div className="flex items-center gap-2">
+                  <TemplateSelector
+                    selectedTemplate={resumeData.template}
+                    onChange={(template) =>
+                      setResumeData((prev) => ({ ...prev, template }))
+                    }
+                  />
+                  <ColorPicker
+                    selectedColor={resumeData.accent_color}
+                    onChange={(color) =>
+                      setResumeData((prev) => ({
+                        ...prev,
+                        accent_color: color,
+                      }))
+                    }
+                  />
+                </div>
                 <div className="flex items-center">
                   {activeSectionIndex > 0 && (
                     <button
@@ -112,11 +133,35 @@ const ResumeBuilder = () => {
                   </button>
                 </div>
               </div>
+
+              {/* Section Form */}
+              <div className="">
+                {activeSection.id === "personal" && (
+                  <PersonalInfoForm
+                    data={resumeData.personal_info}
+                    onChange={(data) =>
+                      setResumeData((prev) => ({
+                        ...prev,
+                        personal_info: data,
+                      }))
+                    }
+                    removeBackground={removeBackground}
+                    setRemoveBackground={setRemoveBackground}
+                  />
+                )}
+              </div>
             </div>
           </div>
 
           {/* Right panel - preview */}
-          <div className=""></div>
+          <div className="lg:col-span-7 max-lg:mt-6">
+            <div></div>
+            <ResumePreview
+              data={resumeData}
+              template={resumeData.template}
+              accentColor={resumeData.accent_color}
+            />
+          </div>
         </div>
       </div>
     </div>
